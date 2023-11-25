78e35fc22c3fc5f59c0c1621384cdbb2f823f5a0
Judicaeld17e <judicaeldjidonou3@gmail.com>
2023-11-25 18:35:39 +0100

    first commit

---
+++ my_numpy_journey_print_shape.py
@@ -0,0 +1,4 @@
+#TASK: Create a function which receives a Numpy array as parameter and prints it shape on stdout.
+import numpy as np
+def my_numpy_journey_print_shape(np_array):
+    print(np_array.shape)
